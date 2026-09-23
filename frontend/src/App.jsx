a243770a3207f78fea6859699148ebd098666ea2
Darren Pereira <darrn.pereira@gmail.com>
2026-09-23 11:17:55 +0530

    Initail commit project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Personal Ai Agent</h1>    
+      <div className='container'>
+        
+        <button>check this out</button>
+      </div>   
+    </>
+  )
+}
+
+export default App
